c74108700b98859c5406f7a264bc4a9767cb1c1d
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-04 23:28:03 +0700

    Memasukkan new node beserta cout dan cin untuk input data urutan nomor mahasiswa dan nama mahasiswa di void add node

--- doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
+++ doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
@@ -13,3 +13,9 @@ struct Node
 Node* START = NULL;
 
 void addNode() {
+	Node* newNode = new Node(); //step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs; //Assign value to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; //Assign value to the data field of the new node
+
