45c0eff4b2ca0678e1a18b9a3057d923e4039bca
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-04 23:58:38 +0700

    Memasukkan current dan previous

--- doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
+++ doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
@@ -91,4 +91,16 @@ void deleteNode()
 		return;
 	}
 
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo; //step 3: get the roll number to be deleted
+
+	if (START == NULL)
+	{
+		cout << "List is empty" << endl;
+		return;
+	}
+
+	current = START; //step 1: start from the first node
+	previous = NULL;
+
 	
