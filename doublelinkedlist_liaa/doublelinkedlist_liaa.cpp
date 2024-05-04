5d7fc3fd849516dfa8db92e34436ed20d1fa516e
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-04 23:57:25 +0700

    Memasukkan cout dan cin dan conditional statement if hingga return pada void delete node

--- doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
+++ doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
@@ -82,4 +82,13 @@ void deleteNode()
 	Node*previous, *current;
 	int rollNo;
 
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo; //step 3: get the roll number to be deleted
+
+	if (START == NULL)
+	{
+		cout << "List is empty" << endl;
+		return;
+	}
+
 	
