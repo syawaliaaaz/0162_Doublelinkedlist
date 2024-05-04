aa651c234a2b14048ee7a1a6178f3d201da9537c
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-04 23:32:25 +0700

    Menggunakan conditional statement if

--- doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
+++ doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
@@ -26,3 +26,11 @@ void addNode() {
 			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
 			return;
 		}
+
+		//if the list is empty, make the new node the START
+		newNode->next = START; //step 3: make the new node point to the first node
+		if (START != NULL) {
+			START->prev = newNode; //step 4: make the first node point to the new node
+		}
+		newNode->prev = NULL; //step 5: make the new node point to NULL
+		START = newNode; //step 6: make the new node the first try mode
