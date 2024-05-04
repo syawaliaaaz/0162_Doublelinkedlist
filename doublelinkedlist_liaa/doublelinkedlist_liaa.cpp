288b6515d3ee94b5831a20ace46b4f36433e2494
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-04 23:44:31 +0700

    Memasukkan looping while pada bool search

--- doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
+++ doublelinkedlist_liaa/doublelinkedlist_liaa.cpp
@@ -68,3 +68,8 @@ bool search(int rollNo, Node** previous, Node** current)
 {
 	*previous = NULL;
 	*current = START;
+	while (*current != NULL && (*current)->noMhs != rollNo)
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
